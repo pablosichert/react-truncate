98e8d22c891d2976a0b32f42000f057c2cf5ca9c
Pablo Sichert <mail@pablosichert.de>
2016-07-19 13:43:47 +0200

    Test Truncate to be a React component

--- test/Truncate.js
+++ test/Truncate.js
@@ -1,3 +1,12 @@
+import { Component } from 'react';
+import unexpected from 'unexpected';
+
+import Truncate from '../src/Truncate';
+
+const expect = unexpected.clone();
+
 describe('<Truncate />', () => {
-    it('should pass', () => {});
+    it('should be a React component', () => {
+        expect(Truncate, 'to be a', Component.constructor);
+    });
 });
