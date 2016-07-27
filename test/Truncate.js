9d2dd209cd60175637dea20f04aab525275f398b
Pablo Sichert <mail@pablosichert.de>
2016-07-27 18:17:30 +0200

    Further test truncating behavior

--- test/Truncate.js
+++ test/Truncate.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-import { createRenderer, renderIntoDocument } from 'react-addons-test-utils';
 import unexpected from 'unexpected';
 import unexpectedReact from 'unexpected-react';
 import unexpectedDOM from 'unexpected-dom';
@@ -7,6 +5,8 @@ import sinon from 'sinon';
 import { jsdom } from 'jsdom';
 import requestAnimationFrame from 'raf';
 import Canvas from 'canvas';
+import React, { Component } from 'react';
+import { createRenderer, renderIntoDocument } from 'react-addons-test-utils';
 import { stripIndent } from 'common-tags';
 
 import Truncate from '../src/Truncate';
@@ -52,27 +52,103 @@ describe('<Truncate />', () => {
         expect(renderer, 'to have rendered', <span />);
     });
 
-    it('should truncate text', () => {
-        sinon.stub(global.window.HTMLDivElement.prototype,
-            'getBoundingClientRect', () => ({ width: 85 })
-        );
+    describe('with a box of 85px mocked out', () => {
+        before(() => {
+            sinon.stub(global.window.HTMLDivElement.prototype,
+                'getBoundingClientRect', () => ({ width: 85 })
+            );
+        });
 
-        let component = renderIntoDocument(
-            <div>
-                <Truncate lines={2} ellipsis='…'>
-                    This text should
-                    stop after here
-                    and not contain the
-                    next lines
-                </Truncate>
-            </div>
-        );
+        after(() => {
+            global.window.HTMLDivElement.prototype.getBoundingClientRect.restore();
+        });
 
-        expect(component.children[0], 'to display text', `
-            This text should
-            stop after here…
-        `);
+        it('should truncate text', () => {
+            let component = renderIntoDocument(
+                <div>
+                    <Truncate lines={2} ellipsis='…'>
+                        This text should
+                        stop after here
+                        and not contain the
+                        next lines
+                    </Truncate>
+                </div>
+            );
 
-        global.window.HTMLDivElement.prototype.getBoundingClientRect.restore();
+            expect(component.children[0], 'to display text', `
+                This text should
+                stop after here…
+            `);
+        });
+
+        it("should not add empty lines when text doesn't fill all lines", () => {
+            let component = renderIntoDocument(
+                <div>
+                    <Truncate lines={4}>
+                        Some short text
+                        over here
+                    </Truncate>
+                </div>
+            );
+
+            expect(component.children[0], 'to display text', `
+                Some short text
+                over here
+            `);
+        });
+
+        it('should not truncate at all if specified in lines prop', () => {
+            let Wrapper = class extends Component {
+                render() {
+                    return (
+                        <div>
+                            <Truncate lines={false}>
+                                Preserve this text
+                                as it was!
+                            </Truncate>
+                        </div>
+                    );
+                }
+            };
+
+            let component = renderIntoDocument(<Wrapper />);
+
+            expect(component, 'to contain', (
+                <span>Preserve this text as it was!</span>
+            ));
+        });
+
+        it('should end truncating when a single word is bigger than its line', () => {
+            let component = renderIntoDocument(
+                <div>
+                    <Truncate lines={2} ellipsis='…'>
+                        Thereisasuperlongwordinthefirstline
+                        so that the next lines won't be
+                        visible
+                    </Truncate>
+                </div>
+            );
+
+            expect(component.children[0], 'to display text', `
+                Thereisasuperl…
+            `);
+        });
+
+        it('should be able to use a react component as ellipsis', () => {
+            let component = renderIntoDocument(
+                <div>
+                    <Truncate lines={2} ellipsis={<a href='#'>… read more</a>}>
+                        I'm curious what
+                        the next lines of
+                        text will say!
+                    </Truncate>
+                </div>
+            );
+
+            expect(component.children[0], 'to display text', `
+                I'm curious what
+                the … read more
+            `);
+        });
     });
 });
