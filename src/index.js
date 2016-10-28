b8e74af2af21e264a2932ca85d6835968ec3ae76
Pablo Sichert <mail@pablosichert.de>
2016-10-28 17:09:20 +0200

    Add demo setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Truncate from '../../src/Truncate.js';
+
+render((
+    <Truncate>
+        Hello world
+    </Truncate>
+), document.body);
